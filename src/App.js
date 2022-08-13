e01052b3034be5123dba8c47d402f016c8320001
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2022-08-13 06:40:30 +0200

    first deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/JS/Header';
+function App() {
+  return (
+    <div className="App">
+      <div className="main-container">
+        <Header/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/JS/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import '../SCSS/Header.scss';
+
+export default function Header() {
+  return (
+    <h1 className='app-header'>My tasks</h1>
+  )
+}
